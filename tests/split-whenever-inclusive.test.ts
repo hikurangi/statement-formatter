abc491d2bb8da67be84f16f80af5a727eb60ea07
Hikurangi <hikurangi@gmail.com>
2024-06-06 07:00:49 +1200

    Remove unused code

--- tests/split-whenever-inclusive.test.ts
+++
@@ -1,9 +0,0 @@
-import splitWheneverInclusive from '../src/lib/split-whenever-inclusive'
-
-test('Splits as expected', () => {
-  const list = [1, 5, 2, 2, 1, 1, 3, 1, 2, 1, 2]
-  const predicate = (item: number) => item === 2
-  const expected = [[1, 5, 2], [2], [1, 1, 3, 1, 2], [1, 2]]
-
-  expect(splitWheneverInclusive(predicate, list)).toEqual(expected)
-})
