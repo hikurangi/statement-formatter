71ccde13f13d281f03bf68b215cacf9974e62980
Hikurangi <hikurangi@gmail.com>
2024-06-06 03:15:10 +1200

    [WIP] Flatten code, improve data retrieval

--- src/main.ts
+++ src/main.ts
@@ -1,100 +1,86 @@
-import { getDocument } from 'pdfjs-dist/legacy/build/pdf.mjs'
+import { PDFDocumentProxy, getDocument } from 'pdfjs-dist/legacy/build/pdf.mjs'
+import { chain, flatten, map, range } from 'ramda'
+import splitWheneverBeforeInclusive from './lib/split-whenever-before-inclusive.js'
+import { KIWIBANK_DATE_FORMAT } from './lib/kiwibank-date-format-regex.js'
 
-import splitWheneverInclusive from './lib/split-whenever-inclusive.js'
+import findSubarraysBy from './lib/find-subarrays-by.js'
+import { KiwibankAccountHeaderZ } from './types/kiwibank-account-headers.js'
+
 const pdfPath = process.argv[2] || './assets/test.pdf'
 
-// Will be using promises to load document, pages and misc data instead of
-// callback.
 const loadingTask = getDocument(pdfPath)
+
+const loadPage = async (pageNum: number, doc: PDFDocumentProxy) => {
+  const page = await doc.getPage(pageNum)
+  console.log('# Page ' + pageNum)
+  const viewport = page.getViewport({ scale: 1 })
+  console.log('Size: ' + viewport.width + 'x' + viewport.height)
+  const content = await page.getTextContent()
+  return content
+}
+
 loadingTask.promise
-  .then(function (doc) {
-    const numPages = doc.numPages
+  .then(async doc => {
+    const data = await doc.getMetadata()
     console.log('# Document Loaded')
-    console.log('Number of Pages: ' + numPages)
-    console.log()
-    let lastPromise // will be used to chain promises
-    lastPromise = doc.getMetadata().then(function (data) {
-      console.log('# Metadata Is Loaded')
-      console.log('## Info')
-      console.log(JSON.stringify(data.info, null, 2))
-      console.log()
-      if (data.metadata) {
-        console.log('## Metadata')
-        console.log(JSON.stringify(data.metadata.getAll(), null, 2))
-        console.log()
-      }
-    })
-    const loadPage = function (pageNum: number) {
-      return doc.getPage(pageNum).then(function (page) {
-        console.log('# Page ' + pageNum)
-        const viewport = page.getViewport({ scale: 1.0 })
-        console.log('Size: ' + viewport.width + 'x' + viewport.height)
-        console.log()
-        return page
-          .getTextContent()
-          .then(function (content) {
-            type Item = (typeof content.items)[number]
-            // iterate through list
-            // first time: item has no
-            // 1 - split 'items' by hasEOL - after each, begin a new array
-            const itemsSplitOnEOL = splitWheneverInclusive<Item>(
-              (item: Item) => 'hasEOL' in item && item.hasEOL === true,
-              content.items
-            )
-            // 2 - extract strings
-            const cleanList = itemsSplitOnEOL.map(row =>
-              row.map(cell => ('str' in cell ? cell.str : ''))
-            )
-            console.log({ cleanList })
-            // Content contains lots of information about the text layout and
-            // styles, but we need only strings at the moment
-            //   const strings = content.items.map(item =>
-            //     'str' in item ? item.str : ''
-            //   )
-            //   console.log('## Text Content')
-            //   console.log(strings)
-            // Release page resources.
-            page.cleanup()
-          })
-          .then(function () {
-            console.log()
-          })
-        return
-      })
+    console.log('Number of Pages: ' + doc.numPages)
+    console.log('## Info')
+    console.log(JSON.stringify(data.info, null, 2))
+    if (data.metadata) {
+      console.log('## Metadata')
+      console.log(JSON.stringify(data.metadata.getAll(), null, 2))
     }
-    // Loading of the first page will wait on metadata and subsequent loadings
-    // will wait on the previous pages.
-    for (let i = 1; i <= numPages; i++) {
-      lastPromise = lastPromise.then(loadPage.bind(null, i))
-    }
-    return lastPromise
+    return doc
   })
-  .then(
-    function () {
-      console.log('# End of Document')
-    },
-    function (err) {
-      console.error('Error: ' + err)
-    }
-  )
-// Process
+  .then(async doc => {
+    const { numPages } = doc
+    const pageNumbers = range(1, numPages + 1)
+    const promises = map(pageNumber => loadPage(pageNumber, doc), pageNumbers)
 
-// 1. Ingest PDF
-// 2. Convert PDF to shitty array
+    const windowSize = 19
 
-// 3. Join all pages to single array (we're doing this in memory 'cause fuck it)
+    const pages = await Promise.all(promises)
+    const items = chain(page => page.items, pages)
+    const itemsSplitBeforeDate = splitWheneverBeforeInclusive(
+      item => 'str' in item && KIWIBANK_DATE_FORMAT.test(item.str),
+      items
+    )
+    const cleanList = itemsSplitBeforeDate.map(row =>
+      row.map(cell => ('str' in cell ? cell.str : ''))
+    )
+    console.log({ cleanList })
+    const itemsWithPossibleMetadataHeaders = cleanList.filter(
+      item => item.length >= windowSize
+    )
+    const contiguousItemsWithPossibleMetadataHeaders = flatten(
+      itemsWithPossibleMetadataHeaders
+    )
 
-// 4. Identify where everything begins
-// 5. identify where everything ends
-// 6. combine rogue multiline rows into normal single rows
-// I think the rule for this is that each new line begins when there is an entry in the 'balance' column
+    // we wanna start splitting stuff from the start of each accountHeaders subarray
+    // then we can terminate after "closing balance"
 
-// - get metadata
+    const accountHeaders = findSubarraysBy(
+      contiguousItemsWithPossibleMetadataHeaders,
+      windowSize,
+      candidate => KiwibankAccountHeaderZ.safeParse(candidate).success
+    )
+    // connect each header to its subsequent account rows,
+    // while pulling off the relevant data (year, account number, nickname)
 
-// 3. Tests to get the bits
+    console.log({ accountHeaders })
+    // return lastPromise
+  })
+  .then(() => {
+    console.log('# End of Document')
+  })
+  .catch(err => {
+    console.error('Error: ' + err)
+  })
 
-// 3. Clean up array
-// 4. Map to csv
-// 5. Write that shit out
+// 2. find where the actual statement starts
 
-// ?. Tests
+// 3. strip out the page numbers
+
+// 1. Identify the different accounts in a given statement using the text block
+// 2. split them out
+// 3. clean up / strip page numbers etc
