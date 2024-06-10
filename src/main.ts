71e43a8557e0216fd3b2e5c214d203592e3dc6f6
Hikurangi <hikurangi@gmail.com>
2024-06-11 03:05:48 +1200

    Tidy

--- src/main.ts
+++ src/main.ts
@@ -14,34 +14,18 @@ import { KiwibankStatementFinalLineZ } from './types/kiwibank-statement-final-li
 import extractSubarraysBetweenWindows from './lib/extract-subarrays-between-windows.js'
 import formatAccountStatement from './lib/format-account-statement.js'
 
-const pdfPath = process.argv[2] || './assets/test.pdf'
-const __dirname = import.meta.dirname
+const pdfPath = process.argv[2] || './input/test.pdf'
 
-const loadingTask = getDocument(pdfPath)
+const __dirname = import.meta.dirname
 
 const loadPage = async (pageNum: number, doc: PDFDocumentProxy) => {
   const page = await doc.getPage(pageNum)
-  console.log('# Page ' + pageNum)
-  const viewport = page.getViewport({ scale: 1 })
-  console.log('Size: ' + viewport.width + 'x' + viewport.height)
   const content = await page.getTextContent()
   return content
 }
 
-loadingTask.promise
-  .then(async doc => {
-    const data = await doc.getMetadata()
-    console.log('# Document Loaded')
-    console.log('Number of Pages: ' + doc.numPages)
-    console.log('## Info')
-    console.log(JSON.stringify(data.info, null, 2))
-    if (data.metadata) {
-      console.log('## Metadata')
-      console.log(JSON.stringify(data.metadata.getAll(), null, 2))
-    }
-    return doc
-  })
-  .then(async doc => {
+getDocument(pdfPath)
+  .promise.then(async doc => {
     const pagePromises = flow(doc.numPages + 1, [
       range(1),
       map(pageNumber => loadPage(pageNumber, doc)),
@@ -52,37 +36,22 @@ loadingTask.promise
     // the meeting could be an email
     // the email could just walk into the ocean
     // and never be seen again
-    const accountStatements = flow(
-      // NOTE: not quite right
-      // <
-      //   Array<TextContent>,
-      //   Array<TextItem | TextMarkedContent>,
-      //   Array<string>,
-      //   Array<Array<string>,
-      //   Array<Array<AccountStatementT>
-      // >
-      await Promise.all(pagePromises),
-      [
-        chain(page => page.items),
-        map((row: TextItem | TextMarkedContent) =>
-          // is there a better way to type R.flow?
-          'str' in row ? row.str : ''
-        ),
-        // All we want to do here is
-        // TODO: separate / abstract the pdf-js type concerns from our internal business logic
-        // this means
-        // moving the below function into 'formatAccountStatement'
-        extractSubarraysBetweenWindows({
-          startWindowSize: 19,
-          endWindowSize: 5,
-          isStartWindow: window =>
-            KiwibankAccountHeaderZ.safeParse(window).success,
-          isEndWindow: window =>
-            KiwibankStatementFinalLineZ.safeParse(window).success,
-        }),
-        map(formatAccountStatement),
-      ]
-    )
+    const accountStatements = flow(await Promise.all(pagePromises), [
+      chain(page => page.items),
+      map((row: TextItem | TextMarkedContent) =>
+        // is there a better way to type R.flow?
+        'str' in row ? row.str : ''
+      ),
+      extractSubarraysBetweenWindows({
+        startWindowSize: 19,
+        endWindowSize: 5,
+        isStartWindow: window =>
+          KiwibankAccountHeaderZ.safeParse(window).success,
+        isEndWindow: window =>
+          KiwibankStatementFinalLineZ.safeParse(window).success,
+      }),
+      map(formatAccountStatement),
+    ])
 
     return accountStatements
   })
@@ -105,7 +74,7 @@ loadingTask.promise
           })
           .on('finish', () => {
             console.log(
-              `${accountNumber}-${statementPeriod} written successfully!`
+              `${accountNumber} - ${statementPeriod} written successfully!`
             )
           })
       }
