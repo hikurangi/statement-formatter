ab3a32db2688dda7e96c8bf59849b71db83509e0
Hikurangi <hikurangi@gmail.com>
2024-06-11 22:50:46 +1200

    Rewrite top level as promise pipeline

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,18 @@ import { writeToPath } from '@fast-csv/format'
 
 import { PDFDocumentProxy, getDocument } from 'pdfjs-dist/legacy/build/pdf.mjs'
 import {
+  TextContent,
   TextItem,
   TextMarkedContent,
 } from 'pdfjs-dist/types/src/display/api.js'
-import { chain, flow, map, range } from 'ramda'
+import { add, chain, flow, map, pipe, prop, range } from 'ramda'
 
 import { kiwibankCSVRowHeaders } from './types/kiwibank-csv-row.js'
 import { KiwibankAccountHeaderZ } from './types/kiwibank-account-headers.js'
 import { KiwibankStatementFinalLineZ } from './types/kiwibank-statement-final-line.js'
 import extractSubarraysBetweenWindows from './lib/extract-subarrays-between-windows.js'
 import formatAccountStatement from './lib/format-account-statement.js'
+import { AccountStatementT } from './types/account-statement.js'
 
 const pdfPath = process.argv[2] || './input/test.pdf'
 
@@ -24,64 +26,59 @@ const loadPage = async (pageNum: number, doc: PDFDocumentProxy) => {
   return content
 }
 
-getDocument(pdfPath)
-  .promise.then(async doc => {
-    const pagePromises = flow(doc.numPages + 1, [
-      range(1),
-      map(pageNumber => loadPage(pageNumber, doc)),
-    ])
+// TODO: can this work point-free with pipe?
+const getDocumentPagePromises = (doc: PDFDocumentProxy) =>
+  flow(doc, [
+    prop('numPages'),
+    add(1),
+    range(1),
+    map(pageNumber => loadPage(pageNumber, doc)),
+  ])
 
-    // TODO: this whole promise could be a pipeline
-    // the pipeline could be a meeting
-    // the meeting could be an email
-    // the email could just walk into the ocean
-    // and never be seen again
-    const accountStatements = flow(await Promise.all(pagePromises), [
-      chain(page => page.items),
-      map((row: TextItem | TextMarkedContent) =>
-        // is there a better way to type R.flow?
-        'str' in row ? row.str : ''
-      ),
-      extractSubarraysBetweenWindows({
-        startWindowSize: 19,
-        endWindowSize: 5,
-        isStartWindow: window =>
-          KiwibankAccountHeaderZ.safeParse(window).success,
-        isEndWindow: window =>
-          KiwibankStatementFinalLineZ.safeParse(window).success,
-      }),
-      map(formatAccountStatement),
-    ])
+const resolveAllPromises = async <T>(promises: Array<Promise<T>>) =>
+  await Promise.all(promises)
 
-    return accountStatements
-  })
-  .then(async accountStatements => {
-    accountStatements.forEach(
-      ({ accountNumber, statementPeriod, statement }) => {
-        const filePath = resolve(
-          // TODO: CLI app
-          // pass in input file
-          // pass in output folder
-          __dirname,
-          '..',
-          '..',
-          'output',
-          `${accountNumber} - ${statementPeriod}.csv`
-        )
-        writeToPath(filePath, [kiwibankCSVRowHeaders, ...statement])
-          .on('error', err => {
-            return console.error(err)
-          })
-          .on('finish', () => {
-            console.log(
-              `${accountNumber} - ${statementPeriod} written successfully!`
-            )
-          })
-      }
+const mapTextContentToAccountStatements = pipe(
+  chain((page: TextContent) => page.items),
+  map((row: TextItem | TextMarkedContent) => ('str' in row ? row.str : '')),
+  extractSubarraysBetweenWindows({
+    startWindowSize: 19,
+    endWindowSize: 5,
+    isStartWindow: window => KiwibankAccountHeaderZ.safeParse(window).success,
+    isEndWindow: window =>
+      KiwibankStatementFinalLineZ.safeParse(window).success,
+  }),
+  map(formatAccountStatement)
+)
+
+const writeAccountsToDisk = (accountStatements: Array<AccountStatementT>) =>
+  accountStatements.forEach(({ accountNumber, statementPeriod, statement }) => {
+    const filePath = resolve(
+      // TODO: CLI app
+      // pass in input file
+      // pass in output folder
+      __dirname,
+      '..',
+      '..',
+      'output',
+      `${accountNumber} - ${statementPeriod}.csv`
     )
+    writeToPath(filePath, [kiwibankCSVRowHeaders, ...statement])
+      .on('error', err => {
+        return console.error(err)
+      })
+      .on('finish', () => {
+        console.log(
+          `${accountNumber} - ${statementPeriod} written successfully!`
+        )
+      })
+  })
 
-    console.log('# End of Document')
-  })
-  .catch(err => {
-    console.error('Error: ' + err)
-  })
+// Pipeline
+getDocument(pdfPath)
+  .promise.then(getDocumentPagePromises)
+  .then(resolveAllPromises)
+  .then(mapTextContentToAccountStatements)
+  .then(writeAccountsToDisk)
+  .then(() => console.log('# End of Document'))
+  .catch(err => console.error('Error: ' + err))
